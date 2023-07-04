bf6cfa9255d6e08fe76c4cefbc8005d10edbeb4e
Andres Marquez <andresdmf55@gmail.com>
2023-07-03 22:13:37 -0500

    Added responsive styles for search component

--- src/scenes/Main/SearchBar/SearchBar.tsx
+++ src/scenes/Main/SearchBar/SearchBar.tsx
@@ -3,7 +3,7 @@ export default function SearchBar(){
     return (
         <div className="search">
             <input type="text" />
-            <img src="./src/assets/images/icon-search.svg" alt="search-icon" />
+            <img src="https://res.cloudinary.com/dp3chx1yj/image/upload/v1688331217/dictonary-api/icon-search_jgvnfe.svg" alt="search-icon" />
         </div>
     )
 }
