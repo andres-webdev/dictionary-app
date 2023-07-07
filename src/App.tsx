69217de490392d75ae6a5ca923b45ee13b87afc1
Andres Marquez <andresdmf55@gmail.com>
2023-07-06 19:50:07 -0500

    Added component, styles for not found definition and other styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Header from './scenes/Header/Header'
+//import DefinitionNotFound from './scenes/Main/Content/DefinitionNotFound'
 import MainContent from './scenes/Main/Content/MainContent'
 import SearchBar from './scenes/Main/SearchBar/SearchBar'
 import './styles/index.css'
@@ -6,11 +7,12 @@ import './styles/index.css'
 function App() {
 
   return (
-    <div className='app dark'> 
+    <div className='app'> 
       <div className='app__container'>
         <Header />
         <SearchBar />
         <MainContent />
+        {/* <DefinitionNotFound /> */}
       </div>
     </div>
   )
