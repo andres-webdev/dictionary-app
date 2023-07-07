f3bb33ed95c7714a47a547c2067083b712b58b12
Andres Marquez <andresdmf55@gmail.com>
2023-07-06 19:46:56 -0500

    Added font menu component, styles and tests

--- src/scenes/Header/Header.tsx
+++ src/scenes/Header/Header.tsx
@@ -1,19 +1,35 @@
+import { useState } from 'react'
 import { IoToggleSharp } from "react-icons/io5";
 import { HiOutlineMoon } from "react-icons/hi";
+import HeaderFontMenu from "./HeaderFontMenu";
 
 export default function Header(){
+
+    const [showFontMenu, setShowFontMenu] = useState(false)
+
+    const handleChangeFontMenuBtn = () => {
+        setShowFontMenu(elem => !elem)
+    }
+
     return (
         <>
             <header>
-                <img src="https://res.cloudinary.com/dp3chx1yj/image/upload/v1688331217/dictonary-api/logo_xgtbef.svg" />
+                <img src="https://res.cloudinary.com/dp3chx1yj/image/upload/v1688331217/dictonary-api/logo_xgtbef.svg" alt="logo-app" />
                 <div> 
-                    <div>
-                        <button className="header__fontBtn">
+                    <div className='header__fontContainer'>
+                        <button 
+                            className="header__fontBtn" 
+                            data-testid="font-menu-btn"
+                            onClick={handleChangeFontMenuBtn}
+                            >
                             <span>Sans Serif</span> 
                             <figure
                                 className="header__iconBtn">
-                                </figure>
+                            </figure>
                         </button>
+                        {
+                            showFontMenu && <HeaderFontMenu />
+                        }
                     </div>
                     <div className="header__toggle">
                         <button>
--- src/scenes/Header/test/testHeader.test.tsx
+++ src/scenes/Header/test/testHeader.test.tsx
@@ -1,6 +1,14 @@
 import {screen, render} from '@testing-library/react'
 import "@testing-library/jest-dom"
 import Header from '../Header'
+import HeaderFontMenu from '../HeaderFontMenu'
+import userEvent from '@testing-library/user-event'
+
+const fontOptions = {
+    first: "Sans serif",
+    second: 'Serif',
+    third: 'Mono'
+}
 
 describe("Header section", () => {
     test("should render the header component", () => {
@@ -14,5 +22,37 @@ describe("Header section", () => {
         expect(fontBtn).toBeInTheDocument()
         expect(toggleBtn).toBeInTheDocument()
         expect(toggleIcon).toBeInTheDocument()
+    }),
+
+    test("should render the font menu card component", () => {
+
+        render(<HeaderFontMenu />)
+
+        const fonts = screen.getAllByRole('heading', { level: 3 })
+
+        const fontsMenu = fonts.map(elem => elem.textContent)
+
+        expect(fonts.length).toBe(3)
+        expect(fontsMenu[0]).toEqual(fontOptions.first)
+        expect(fontsMenu[1]).toEqual(fontOptions.second)
+        expect(fontsMenu[2]).toEqual(fontOptions.third)
+    }),
+
+    test("should render the font menu card component after the user click the menu botton", async () => {
+
+        const user = userEvent.setup()
+
+        render(<Header />)
+
+        const buttons = screen.getByTestId('font-menu-btn')
+        const fontMenu = screen.queryByTestId('fonts-menu')
+
+        expect(fontMenu).not.toBeInTheDocument()
+
+        await user.click(buttons)
+
+        const showFontMenu = screen.getByTestId('fonts-menu')
+
+        expect(showFontMenu).toBeInTheDocument()
     })
 }) 
