96cb6465b75ce31b56cac234d55028bbe9463598
Andres Marquez <andresdmf55@gmail.com>
2023-07-08 17:54:00 -0500

    Fixed deploy and test config

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
     setupFiles: ['./jest.setup.js'],
     transformIgnorePatterns: [],
 
-    // ModuleNameMapper sólo si ocupamos importar CSS en nuestros componentes para el testing
-    /* moduleNameMapper: {
-        '\\.(css|less)$': '<rootDir>/tests/mocks/styleMock.js',
-    }, */
+    //ModuleNameMapper sólo si ocupamos importar CSS en nuestros componentes para el testing
+    moduleNameMapper: {
+        '\\.(css|less|sass|scss)$': '<rootDir>/src/test/styleMock.js',
+    },
 }
